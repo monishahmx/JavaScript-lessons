f086a075a43b1a38f1a01c539a8a688ad7780ef9
MONI <127129582+monishahmx@users.noreply.github.com>
2026-09-25 15:58:38 +0530

    arrow functions

---
+++ JS lessons/ARROW FUNCTIONS/Af.js
@@ -0,0 +1,32 @@
+//ARROW FUNCTIONS IN JS
+
+//ARROW DECLAIRATION SYNTAX
+
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(1, 2));
+
+//EVEN SIMPLIER WAY
+//IMPLICIT RESTUEN
+
+const subtract = (m, n) => m - n;
+console.log(subtract(48, 33));
+
+//single param
+//can leave off () witha a single param
+
+const double = (a) => a * 3;
+console.log(double(9));
+
+//RETURNING AN OBJECT
+//adding () is important when arrowing the function
+
+const driver = () => ({
+  name: `max verstappen`,
+  team: `RED BULL RACING`,
+  ID: 3,
+});
+console.log(driver());
+
+//IMMEDIATLEY INVOKED FUNCTION experssions
